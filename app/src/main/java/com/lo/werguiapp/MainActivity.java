85f27ecbc99420fbb7b600e79aecfd857290eba1
iso <gayeiso11@gmail.com>
2019-04-20 13:16:46 +0000

    ?

--- app/src/main/java/com/lo/werguiapp/MainActivity.java
+++ app/src/main/java/com/lo/werguiapp/MainActivity.java
@@ -2,12 +2,32 @@ package com.lo.werguiapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText Cin,Password;
+    private Button Login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Cin=(EditText)findViewById(R.id.edCin);
+        Password=(EditText)findViewById(R.id.edMdp);
+        Login=(Button) findViewById(R.id.btnLogin);
+
+    }
+
+    public void Login(View view){
+        Background background=new Background(this);
+        String password=Password.getText().toString();
+        String cin=Cin.getText().toString();
+        String type="login";
+        background.execute(type,cin,password);
+
     }
 }
